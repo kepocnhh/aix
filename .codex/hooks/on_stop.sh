0fb047ddb4afa02ed02b03ce365bacaf683ba5fd
Stanley Wintergreen <kepocnhh@protonmail.com>
2026-05-07 23:35:20 +0300

    stop

--- .codex/hooks/on_stop.sh
+++ .codex/hooks/on_stop.sh
@@ -8,6 +8,11 @@ if test $? -ne 0; then
  echo 'Could not get session ID!' >&2
  echo '{"continue":false}'; exit 1; fi
 
+CODEX_TURN_ID=$(printf '%s' "${JSON_INPUT}" | yq -e -r .turn_id)
+if test $? -ne 0; then
+ echo 'Could not get turn ID!' >&2
+ echo '{"continue":false}'; exit 1; fi
+
 CODEX_WORKDIR=$(printf '%s' "${JSON_INPUT}" | yq -e -r .cwd)
 if test $? -ne 0; then
  echo 'Could not get workdir!' >&2
@@ -32,8 +37,6 @@ if test -d "${ISSUER}"; then
 else
  echo "No dir \"${ISSUER}\"." >&2
 fi
-
-CODEX_TURN_ID=$(printf '%s' "${JSON_INPUT}" | yq -r .turn_id) # todo
 
 CODEX_TRANSCRIPT_PATH=$(printf '%s' "${JSON_INPUT}" | yq -r '.transcript_path // ""')
 if [[ ! -f "${CODEX_TRANSCRIPT_PATH}" ]]; then
@@ -64,6 +67,23 @@ if test -d "${ISSUER}"; then
  printf '%s' "${CODEX_RESPONSE}" > "${ISSUER}/${FILE_DIR}/codex-${POINTER}.md"
 else
  echo "No dir \"${ISSUER}\"." >&2
+fi
+
+ISSUER="${CODEX_WORKDIR}/.excluded/yml/codex/codex-${CODEX_SESSION_ID}-${CODEX_TURN_ID}.yml"
+
+if test -f "${ISSUER}"; then
+ ACTUAL_SESSION_ID=$(printf '%s' "${JSON_INPUT}" | yq -e -r .session_id)
+ if [[ "${CODEX_SESSION_ID}" != "${ACTUAL_SESSION_ID}" ]]; then
+  echo "Actual session id \"${ACTUAL_SESSION_ID}\", but expected \"${CODEX_SESSION_ID}\"!" >&2
+  echo '{"continue":false}'; exit 1; fi
+ ACTUAL_TURN_ID=$(printf '%s' "${JSON_INPUT}" | yq -e -r .turn_id)
+ if [[ "${CODEX_TURN_ID}" != "${ACTUAL_TURN_ID}" ]]; then
+  echo "Actual turn id \"${ACTUAL_TURN_ID}\", but expected \"${CODEX_TURN_ID}\"!" >&2
+  echo '{"continue":false}'; exit 1; fi
+ CODEX_RESPONSE="${CODEX_RESPONSE}" \
+  yq -M -i -p yml -o yml '.last_assistant_message=strenv(CODEX_RESPONSE)' "${ISSUER}"
+else
+ echo "No file \"${ISSUER}\"." >&2
 fi
 
 echo '{"continue":true}'
