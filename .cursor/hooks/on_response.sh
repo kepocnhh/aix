675debfd49dfc0dca36ad5f5fb860378b73e9fb3
Stanley Wintergreen <kepocnhh@protonmail.com>
2026-05-21 01:44:52 +0300

    wip

--- .cursor/hooks/on_response.sh
+++ .cursor/hooks/on_response.sh
@@ -30,9 +30,26 @@ if test -d "${ISSUER}"; then
  printf '%s' "${JSON_INPUT}" > "${ISSUER}/${FILE_DIR}/${AI_NAME}-${POINTER}.json"
 fi
 
+AI_RESPONSE=$(printf '%s' "${JSON_INPUT}" | yq -r -p=json -o=json '.text // ""')
+if test -z "${AI_RESPONSE}"; then
+ echo "No response!" >&2; exit 1; fi
+
 ISSUER="${AI_WORKDIR}/.excluded/md/${AI_NAME}"
 
 if test -d "${ISSUER}"; then
  mkdir -p "${ISSUER}/${FILE_DIR}"
- printf '%s' "${JSON_INPUT}" | yq -r -p=json -o=json .text > "${ISSUER}/${FILE_DIR}/${AI_NAME}-${POINTER}.md"
+ printf '%s' "${AI_RESPONSE}" > "${ISSUER}/${FILE_DIR}/${AI_NAME}-${POINTER}.md"
+fi
+
+ISSUER="${AI_WORKDIR}/.excluded/yml/${AI_NAME}/${AI_NAME}-${AI_SESSION_ID}-${AI_TURN_ID}.yml"
+
+if test -f "${ISSUER}"; then
+ ACTUAL_SESSION_ID=$(yq -r -p=yml -o=json .session_id "${ISSUER}")
+ if [[ "${AI_SESSION_ID}" != "${ACTUAL_SESSION_ID}" ]]; then
+  echo "Actual session id \"${ACTUAL_SESSION_ID}\", but expected \"${AI_SESSION_ID}\"!" >&2; exit 1; fi
+ ACTUAL_TURN_ID=$(yq -r -p=yml -o=json .turn_id "${ISSUER}")
+ if [[ "${AI_TURN_ID}" != "${ACTUAL_TURN_ID}" ]]; then
+  echo "Actual turn id \"${ACTUAL_TURN_ID}\", but expected \"${AI_TURN_ID}\"!" >&2; exit 1; fi
+ AI_RESPONSE="${AI_RESPONSE}" \
+  yq -M -i -p yml -o yml '.response=strenv(AI_RESPONSE)' "${ISSUER}"
 fi
