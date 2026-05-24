472fb27aaf5593aaadf7c409ed9276e0dae25a81
Stanley Wintergreen <kepocnhh@protonmail.com>
2026-05-24 23:15:51 +0300

    response

--- .cursor/hooks/on_response.sh
+++ .cursor/hooks/on_response.sh
@@ -29,35 +29,28 @@ AI_TURN_ID=$(printf '%s' "${JSON_INPUT}" | yq -eMr -p=json -o=json .generation_i
 if test $? -ne 0; then
  echo 'Could not get generation ID!' >&2; exit 1; fi
 
-POINTER="$(TZ='utc' LC_ALL=C date +%Y%m%d%H%M%S)-${AI_SESSION_ID:0:8}"
-
-ISSUER="${AI_WORKDIR}/.excluded/json/${AI_NAME}"
-
-if test -d "${ISSUER}"; then
- mkdir -p "${ISSUER}/${FILE_DIR}"
- printf '%s' "${JSON_INPUT}" > "${ISSUER}/${FILE_DIR}/${AI_NAME}-${POINTER}.json"
-fi
-
 AI_RESPONSE=$(printf '%s' "${JSON_INPUT}" | yq -r -p=json -o=json '.text // ""')
 if test -z "${AI_RESPONSE}"; then
  echo 'No response!' >&2; exit 1; fi
 
 ISSUER="${AI_WORKDIR}/.excluded/md/${AI_NAME}"
 
+POINTER="$(TZ='utc' LC_ALL=C date +%Y%m%d%H%M%S)-${AI_SESSION_ID:0:8}"
+
 if test -d "${ISSUER}"; then
  mkdir -p "${ISSUER}/${FILE_DIR}"
  printf '%s' "${AI_RESPONSE}" > "${ISSUER}/${FILE_DIR}/${AI_NAME}-${POINTER}.md"
 fi
 
-ISSUER="${AI_WORKDIR}/.excluded/yml/${AI_NAME}/${AI_NAME}-${AI_SESSION_ID}-${AI_TURN_ID}.yml"
+ISSUER="${AI_WORKDIR}/.excluded/json/${AI_NAME}/${AI_NAME}-${AI_SESSION_ID}-${AI_TURN_ID}.json"
 
 if test -f "${ISSUER}"; then
- ACTUAL_SESSION_ID=$(yq -r -p=yml -o=json .session_id "${ISSUER}")
+ ACTUAL_SESSION_ID=$(yq -r -p=json -o=json .session_id "${ISSUER}")
  if [[ "${AI_SESSION_ID}" != "${ACTUAL_SESSION_ID}" ]]; then
   echo "Actual session id \"${ACTUAL_SESSION_ID}\", but expected \"${AI_SESSION_ID}\"!" >&2; exit 1; fi
- ACTUAL_TURN_ID=$(yq -r -p=yml -o=json .turn_id "${ISSUER}")
+ ACTUAL_TURN_ID=$(yq -r -p=json -o=json .turn_id "${ISSUER}")
  if [[ "${AI_TURN_ID}" != "${ACTUAL_TURN_ID}" ]]; then
   echo "Actual turn id \"${ACTUAL_TURN_ID}\", but expected \"${AI_TURN_ID}\"!" >&2; exit 1; fi
  STR_VALUE="${AI_RESPONSE}" \
-  yq -i -p=yml -o=yml '.response=strenv(STR_VALUE)' "${ISSUER}"
+  yq -i -p=json -o=json '.response=strenv(STR_VALUE)' "${ISSUER}"
 fi
