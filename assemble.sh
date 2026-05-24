8d1405df3b5693835b1905cfcbc86a9f0276a03e
Stanley Wintergreen <kepocnhh@protonmail.com>
2026-05-24 23:03:09 +0300

    assemble

---
+++ assemble.sh
@@ -0,0 +1,25 @@
+#!/usr/local/bin/bash
+
+REP_OWNER='kepocnhh'
+REP_NAME='aix'
+VERSION='0.0.1'
+
+mkdir 'build'
+mkdir -p 'build/yml'
+ISSUER='build/yml/metadata.yml'
+printf '%s' "repository:
+ owner: '${REP_OWNER}'
+ name: '${REP_NAME}'
+version: '${VERSION}'" > "${ISSUER}"
+
+if [[ ! -s 'LICENSE' ]]; then
+ echo 'No license!' >&2; exit 1; fi
+
+if [[ ! -s 'README.md' ]]; then
+ echo 'No readme!' >&2; exit 1; fi
+
+mkdir -p 'build/zip'
+ISSUER="build/zip/${REP_NAME}-${VERSION}.zip"
+zip -r "${ISSUER}" '.codex' '.cursor' 'LICENSE' 'README.md'
+if test $? -ne 0; then
+ echo 'Zip error!' >&2; exit 1; fi
