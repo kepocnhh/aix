322f06affd89284a157abddd31b9ceb62e9f8141
Stanley Wintergreen <kepocnhh@protonmail.com>
2026-05-25 21:14:26 +0300

    tests

--- src/test/bash/cursor/on_response_test.sh
+++ src/test/bash/cursor/on_response_test.sh
@@ -34,6 +34,18 @@ ACTUAL_VALUE="$(<"${STDERR}")"
 if test "${ACTUAL_VALUE}" != "Workdir \"${TMP_DIR}\" error!"; then
  echo "Actual value(${#ACTUAL_VALUE}) is: \"${ACTUAL_VALUE}\"!" >&2; exit 1; fi
 
+:> "${STDERR}"
+
+TMP_DIR="$(mktemp)"
+rm "${TMP_DIR}"
+ln -sf "${TMP_DIR}" "${TMP_DIR}"
+AI_WORKDIR="${TMP_DIR}" "${SCRIPT}" 2>"${STDERR}"; CODE=$?
+if test "${CODE}" != '1'; then
+ echo "Code(${CODE}) error!" >&2; exit 1; fi
+ACTUAL_VALUE="$(<"${STDERR}")"
+if test "${ACTUAL_VALUE}" != 'Realpath workdir error!'; then
+ echo "Actual value(${#ACTUAL_VALUE}) is: \"${ACTUAL_VALUE}\"!" >&2; exit 1; fi
+
 echo 'Not implemented!' >&2; exit 1 # todo
 
 rm "${STDERR}"
